e4f16997175c1334a23e121deafa89076612dec4
Shunsuke Shimura <shunsuke.shimura@satori.team>
2022-10-17 02:59:16 +0000

    create installer.sh and modified README.md

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# git-installer-scripts
+# git-installer-script-for-ubuntu20.04
 scripts for installing git from source
+
+## Requriements
+- Ubuntu 20.04 LTS
+- wget
+- tar
+
+## Usage
+### with default configurations
+- `./installer.sh`
+### with custom configurations
+- `GIT_PREFIX={path_to_git_prefix} ./installer.sh`
+
+## Configurations
+### Environment Variables
+- `GIT_PREFIX` : prefix path to install Git (default to `/usr/local`)
+- `ZLIB_PREFIX` : prefix path to install Zlib (default to `${GIT_PREFIX}`)
---
+++ installer.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Target OS: Ubuntu 20.04
+GIT_PREFIX=${GIT_PREFIX:-/usr/local/bin}
+GIT_VERSION="2.37.3"
+
+ZLIB_PREFIX=${ZLIB_PREFIX:-${GIT_PREFIX}}
+ZLIB_VERSION="1.2.12"
+
+mkdir -p /tmp/git-installer
+cd /tmp/git-installer
+sudo apt update
+# install dependencies
+# do not use `sudo apt-get build-dep git`
+# because deb-src is commented out by default in /etc/apt/sources.list
+sudo apt-get install -y build-essential dh-autoreconf libcurl4-gnutls-dev libexpat1-dev \
+  gettext libz-dev libssl-dev
+# libraries for generating documents
+sudo apt-get install -y asciidoc xmlto docbook2x
+# manage installed documentation in info format
+sudo apt-get install -y install-info
+
+
+# download, build, install zlib (required library)
+wget -O zlib-${ZLIB_VERSION}.tar.gz https://zlib.net/zlib-${ZLIB_VERSION}.tar.gz
+tar xzvf zlib-${ZLIB_VERSION}.tar.gz
+cd zlib-${ZLIB_VERSION}
+prefix=${ZLIB_PREFIX} ./configure
+sudo make install
+
+# download, build, install Git
+wget -O git-${GIT_VERSION}.tar.gz https://github.com/git/git/archive/refs/tags/v${GIT_VERSION}.tar.gz
+tar xzvf git-${GIT_VERSION}.tar.gz
+cd git-${GIT_VERSION}
+make configure
+prefix=${GIT_PREFIX} ./configure
+ZLIB_PATH=${ZLIB_PREFIX} sudo make install
